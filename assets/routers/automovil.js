53bd79082ee63524491d2df5668adb1c9c372b66
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-08-15 16:55:43 -0500

    feat(MongoDB): :sparkles: Get - Cantidad Total Automoviles - Dto - Jwt
    Se desarrolla EndPoint que permite mostrar la cantidad total de automóviles disponibles en cada sucursal

--- assets/routers/automovil.js
+++ assets/routers/automovil.js
@@ -3,8 +3,39 @@ import {conx} from '../db/atlas.js';
 import {limitApi} from '../limit/limit.js';
 const appAutomovil = express();
 
-appAutomovil.get("/", async (req,res)=>{
-    res.send("Hola Mundo");
+appAutomovil.get("/total",limitApi(),  async(req,res)=>{
+    let db = await conx();
+    let alquiler = db.collection("sucursal");
+    let query = [
+        {
+            $lookup: {
+              from: "sucursal_automovil",
+              localField: "ID_Sucursal",
+              foreignField: "ID_Sucursal",
+              as: "data_Sucursal"
+            }
+        },
+        {
+            $unwind: "$data_Sucursal"
+        },
+        {
+            $group: {
+              _id: "$data_Sucursal.ID_Sucursal",
+              total_Automoviles: {$sum: "$data_Sucursal.Cantidad_Disponible"}
+            }
+        },
+        {
+            $project: {
+              "_id": 0,
+              "ID_Sucursal": "$_id",
+              "Nombre": 1,
+              "Total_Automoviles": "$total_Automoviles"
+            }
+        },
+        { $sort: { ID_Sucursal: 1 } }
+    ];
+    let result = await alquiler.aggregate(query).toArray();
+    res.send(result); 
 })
 
 export default appAutomovil;
